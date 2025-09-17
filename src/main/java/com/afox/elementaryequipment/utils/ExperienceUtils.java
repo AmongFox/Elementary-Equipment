20ba97fdc71c3955f5a4c84c05b27bd11c06b4c0
Владислав Буррасков <bourraska@gmail.com>
2025-09-18 01:14:54 +0300

    Fix ExperienceUtils

--- src/main/java/com/afox/elementaryequipment/utils/ExperienceUtils.java
+++ src/main/java/com/afox/elementaryequipment/utils/ExperienceUtils.java
@@ -13,23 +13,53 @@ public class ExperienceUtils {
             return true;
         }
 
-        if (player.totalExperience >= xpCost) {
-            player.addExperience(-xpCost);
-        } else {
-            if (world instanceof ServerWorld serverWorld) {
-                serverWorld.playSound(
-                        null,
-                        player.getX(), player.getY(), player.getZ(),
-                        SoundEvents.BLOCK_ANVIL_LAND,
-                        SoundCategory.PLAYERS,
-                        0.5F,
-                        1.0F
-                );
-            }
+        int totalXP = getTotalPlayerXP(player);
 
-            player.sendMessage(Text.literal("§4Недостаточно опыта!"), true);
-            return false;
+        if (totalXP >= xpCost) {
+            player.addExperience(-xpCost);
+            return true;
         }
-        return true;
+
+        if (world instanceof ServerWorld serverWorld) {
+            serverWorld.playSound(
+                    null,
+                    player.getX(), player.getY(), player.getZ(),
+                    SoundEvents.BLOCK_ANVIL_LAND,
+                    SoundCategory.PLAYERS,
+                    0.5F,
+                    1.0F
+            );
+        }
+
+        player.sendMessage(Text.literal("§4Недостаточно опыта! Нужно: " + (xpCost - totalXP) + " очков"), true);
+        return false;
+    }
+
+    public static int getTotalPlayerXP(PlayerEntity player) {
+        int playerLevel = player.experienceLevel;
+        int totalXP = getXPForLevel(playerLevel);
+        totalXP += Math.round(player.experienceProgress * getXpToNextLevel(playerLevel));
+        return totalXP;
+    }
+
+    private static int getXPForLevel(int level) {
+        if (level <= 0) return 0;
+        if (level <= 16) {
+            return (level * level) + (6 * level);
+        } else if (level <= 31) {
+            return (int) ((2.5 * level * level) - (40.5 * level) + 360);
+        } else {
+            return (int) ((4.5 * level * level) - (162.5 * level) + 2220);
+        }
+    }
+
+    private static int getXpToNextLevel(int level) {
+        if (level <= 15) {
+            return 2 * level + 7;
+        } else if (level <= 30) {
+            return 5 * level - 38;
+        } else {
+            return 9 * level - 158;
+        }
     }
 }
